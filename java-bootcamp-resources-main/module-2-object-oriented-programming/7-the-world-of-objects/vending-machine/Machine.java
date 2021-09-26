e4d64e3e72cbdbea40ecea30da8233c36e210083
rgoshen <rick.goshen@gmail.com>
2021-09-26 11:38:26 -0700

    completed section 7 oop challenge

--- java-bootcamp-resources-main/module-2-object-oriented-programming/7-the-world-of-objects/vending-machine/Machine.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/7-the-world-of-objects/vending-machine/Machine.java
@@ -1,16 +1,59 @@
 public class Machine {
-    
-    
-    
+
+    private Item[][] items;
+
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            };
+        }
+    }
+
+    public Item getItem(int row, int col) {
+        return new Item(this.items[row][col]);
+    }
+
+    public void setItem(Item item, int row, int col) {
+        this.items[row][col] = new Item(item);
+    }
+
     /**
      * Function name – dispense
      * @param row (int)
      * @param spot (int)
      * @return (boolean)
-     * 
+     *
      * Inside the function:
      *  1. Checks if the requested item has a quantity bigger than 0.
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int col) {
+        if (this.items[row][col].getQuantity() > 0) {
+            int quantity = this.items[row][col].getQuantity();
+            this.items[row][col].setQuantity(quantity - 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    public String toString() {
+        String temp = "";
+
+        for (int i = 0; i < this.items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < this.items[i].length; j++) {
+                temp += this.items[i][j].toString();
+            }
+            temp += "\n\n";
+        }
+
+        temp += "\t************************************************";
+
+        return temp;
+    }
+
 }
--- java-bootcamp-resources-main/module-2-object-oriented-programming/7-the-world-of-objects/vending-machine/Main.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/7-the-world-of-objects/vending-machine/Main.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int option = 1;
 
         System.out.println("\t************************************************");
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
-        
-        /* Item[][] items = new Item[][] {
-        /     { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
-        /     { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
-        /     { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
-        / };
-        */
+
+        Item[][] items = new Item[][] {
+            {
+                new Item("Pepsi", 1.99, 3),
+                new Item("Fresca", 1.49, 3),
+                new Item("Brisk", 2.49, 2)
+            },
+            {
+                new Item("Fanta", 1.99, 2),
+                new Item("Barq's", 1.49, 2),
+                new Item("A & W", 2.49, 3)
+            },
+            {
+                new Item("Crush", 1.99, 2),
+                new Item("C-Cola", 1.49, 2),
+                new Item("Berry", 2.49, 1)
+            }
+        };
+
+        Machine machine = new Machine(items);
+
+        System.out.println(machine);
+
+        while (option == 1) {
+            System.out.print("\nPick a row and a spot in the row: ");
+            int row = scan.nextInt();
+            int spot = scan.nextInt();
+
+            if (machine.dispense(row, spot)) {
+                System.out.println("\n" + machine);
+                System.out.print("\nEnjoy your drink! Press 1 to purchase another: ");
+            } else {
+                System.out.print("\nSorry, we're out of this item. Press 1 to purchase another: ");
+            }
+            option = scan.nextInt();
+        }
     }
 }
