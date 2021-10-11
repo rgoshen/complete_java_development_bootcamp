2becd72f098012845dcae6c0e6568ed177d2939b
rgoshen <rick.goshen@gmail.com>
2021-10-10 17:40:53 -0700

    finished TDD for cart app

--- java-bootcamp-resources-main/module-2-object-oriented-programming/9-unit-testing/Workbooks/shopping-cart/src/main/models/Cart.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/9-unit-testing/Workbooks/shopping-cart/src/main/models/Cart.java
@@ -1,6 +1,8 @@
 package src.main.models;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Cart {
     ArrayList<Item> items;
@@ -17,10 +19,6 @@ public class Cart {
         this.items.set(index, new Item(item));
     }
 
-
-
-
-    
     public String toString() {
         String temp = "";
         for (int i = 0; i < this.items.size(); i++) {
@@ -28,6 +26,106 @@ public class Cart {
             temp += "\n";
         }
         return temp;
+    }
+
+    /**
+     * Function name: add
+     * @param item 
+     * @return (boolean) <-------
+     * 
+     * Inside the function:
+     *   1. adds an Item object
+     *   2. returns true <-------
+     */
+    public boolean add(Item item) {
+        if (this.items.contains(item)) {
+            return false;
+        }
+
+        this.items.add(new Item(item));
+        return true;
+    }
+
+    /**
+    * Function name: remove
+    * @param name
+    * 
+    * Inside the function:
+    *   1. loop runs through the size of the ArrayList.
+    *   2. removes the item that matches the name passed in. 
+    */
+    public void remove(String name) {
+        if (this.items.isEmpty()) {
+            throw new IllegalStateException("INVALID STATE");
+        }
+        this.items.removeIf((item) -> item.getName().equals(name));
+    }
+  
+    /**
+    * Function name: getSubtotal
+    * @return double
+    * 
+    * Inside the function:
+    *   1. loop runs through the size of the ArrayList.
+    *   2. adds the price field of each Item object. 
+    *   3. returns the sum
+    */
+    public double getSubtotal() {
+        return this.items.stream().mapToDouble((item) -> item.getPrice()).sum();
+    }
+  
+    /**
+    * Function name: getTax
+    * @param double (subtotal)
+    * @return double
+    * 
+    * Inside the function:
+    *   1. calculates tax
+    *   2. rounds to two decimal places
+    *   3. returns the result
+    */
+    public double getTax(double subtotal) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return Double.parseDouble(df.format(subtotal * 0.13));
+    }
+  
+    /**
+    * Function name: getTotal
+    * @param double (subtotal)
+    * @param double (tax)
+    * @return (double)
+    * 
+    * Inside the function:
+    *   1. returns subtotal + tax
+    */
+    public double getTotal(double subtotal, double tax) {
+        return subtotal + tax;
+    }
+
+    public String checkout() {
+        if (this.items.isEmpty()) {
+            throw new IllegalStateException("IVALID STATE");
+        }
+        return "\tRECEIPT\n\n" +
+            "\tSubtotal: $" + getSubtotal() + "\n" +
+            "\tTax: $" + getTax(getSubtotal()) + "\n" +
+            "\tTotal: $" + getTotal(getSubtotal(), getTax(getSubtotal())) + "\n";
+    }
+
+    /**
+     * Function name: contains
+    * @param item
+    * @return (boolean)
+    * 
+    * Inside the function:
+    *    1. checks if items list contains() item.
+    */
+    public boolean contains(Item item) {
+        return this.items.contains(item);
+    }
+
+    public void clear() {
+        this.items.clear();
     }
 
 }
--- java-bootcamp-resources-main/module-2-object-oriented-programming/9-unit-testing/Workbooks/shopping-cart/src/test/CartTest.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/9-unit-testing/Workbooks/shopping-cart/src/test/CartTest.java
@@ -1,9 +1,66 @@
 package src.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import src.main.models.Cart;
+import src.main.models.Item;
 
 public class CartTest {
 
     Cart cart;
-    
+
+    @Before
+    public void setup() {
+        cart = new Cart();
+        cart.add(new Item("Pepsi", 1.99));
+        cart.add(new Item("Crush", 1.99));
+    }
+
+    @Test
+    public void itemAddedTest() {
+        assertTrue(cart.contains(new Item("Pepsi", 1.99)));
+    }
+
+    @Test
+    public void skipsDuplicateTest() {
+        assertFalse(cart.add(new Item("Crush", 1.99)));
+    }
+
+    @Test
+    public void removedItemTest() {
+        cart.remove("Pepsi");
+        assertFalse(cart.contains(new Item("Pepsi", 1.99)));
+    }
+
+    @Test
+    public void subTotalIsValidTest() {
+        assertEquals(3.98, cart.getSubtotal());
+    }
+
+    @Test
+    public void taxIsValidTest() {
+        assertEquals(3.9, cart.getTax(30));
+    }
+
+    @Test
+    public void totalIsValidTest() {
+        assertEquals(96.67, cart.getTotal(85.55, 11.12));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void invalidRemoveStateTest() {
+        cart.clear();
+        cart.remove("Pepsi");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void invalidCheckoutStateTest() {
+        cart.clear();
+        cart.checkout();
+    }
 }
