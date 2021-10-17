e813bbe7abd9728e45e77e538b06a446258714f0
rgoshen <rick.goshen@gmail.com>
2021-10-16 17:33:05 -0700

    finished comparable lesson

--- java-bootcamp-resources-main/module-2-object-oriented-programming/10-inheritance/products/Main.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/10-inheritance/products/Main.java
@@ -1,11 +1,75 @@
+import java.util.Arrays;
+
+import models.Pants;
+import models.Product;
+import models.Shirt;
+import models.Shirt.Size;
+
 public class Main {
-  
+
     static final String FILE_NAME = "products.txt";
 
     public static void main(String[] args) {
 
+        // Shirt shirt = new Shirt(Shirt.Size.MEDIUM, 5.99, "blue", "Java Vuitton");
+        // Pants pants = new Pants(40, 10.99, "blue", "Java Klein");
+        // Shirt shirt2 = new Shirt(shirt);
+        // Pants pants2 = new Pants(pants);
+        // Pants pants3 = new Pants(40, 104.99, "black", "Java Levi");
+
+        // pants2.setPrice(59.99);
+        // shirt2.setSize(Size.LARGE);
+        // shirt2.setPrice(7.99);
+        // System.out.println(shirt.equals(shirt2));
+        // System.out.println("Shirt: " + shirt.hashCode());
+        // System.out.println("Shirt2: " + shirt2.hashCode());
+        // System.out.println(pants.equals(pants2));
+
+        // System.out.println(shirt);
+        // System.out.println();
+        // System.out.println(shirt2);
+        // System.out.println();
+        // pants2.discount();
+        // System.out.println(pants);
+        // System.out.println();
+        // System.out.println(pants2);
+        // System.out.println();
+
+        // shirt.fold();
+        // System.out.println();
+        // pants.fold();
+
+        // System.out.println(pants.compareTo(pants3));
+
+        Product[] products = new Product[] {
+            new Pants(32, 24.99, "Blue", "JAVA KLEIN"),
+            new Shirt(Shirt.Size.MEDIUM, 22.99, "Black", "CHANEL"),
+            new Pants(34, 104.99, "Red", "JANGLER"),
+            new Shirt(Shirt.Size.SMALL, 13.99, "Orange", "GEORGE"),
+            new Pants(30, 119.99, "Grey", "FENDI"),
+            new Shirt(Shirt.Size.LARGE, 34.99, "Blue", "ECKO"),
+            new Pants(30, 129.99, "Red", "VERSACE"),
+            new Shirt(Shirt.Size.SMALL, 22.99, "Beige", "ZARA"),
+            new Pants(29, 99.99, "Dark", "JANGLER"),
+            new Shirt(Shirt.Size.SMALL, 19.99, "Red", "NIKE"),
+            new Pants(26, 24.99, "Indigo", "BELSTAFF"),
+            new Shirt(Shirt.Size.LARGE, 29.99, "Blue", "ADIDAS"),
+            new Pants(34, 104.99, "Red", "JANGLER"),
+        };
+
+        printArray(products);
+        Arrays.sort(products);
+        System.out.println("Sorted:");
+        printArray(products);
+
     }
-  
+    
+    public static void printArray(Product[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
     /**
      * Function Name: getData
      * @return Product[]
--- java-bootcamp-resources-main/module-2-object-oriented-programming/10-inheritance/products/models/Shirt.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/10-inheritance/products/models/Shirt.java
@@ -1,1 +1,64 @@
 package models;
+
+import java.util.Objects;
+
+public class Shirt extends Product {
+    public enum Size {
+        SMALL, MEDIUM, LARGE
+    }
+    private Size size;
+
+    public Shirt(Size size, double price, String color, String brand) {
+        super(price, color, brand);
+        this.size = size;
+    }
+
+    public Shirt(Shirt source) {
+        super(source);
+        this.size = source.size;
+    }
+
+    public Size getSize() {
+        return this.size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public void fold() {
+        System.out.println("Lay shirt on flat surface");
+        System.out.println("Fold shirt sideways");
+        System.out.println("Bring the sleeves in");
+        System.out.println("Fold from bottom up");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Shirt)) {
+            return false;
+        }
+        Shirt shirt = (Shirt) o;
+        return size.equals(shirt.size) && super.getBrand() == shirt.getBrand() && super.getColor() == shirt.getColor()
+                && super.getPrice() == shirt.getPrice();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, super.getBrand(), super.getColor(), super.getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " size='" + getSize() + "'" +
+            " price='" + super.getPrice() + "'" +
+            " color='" + super.getColor() + "'" +
+            " brand='" + super.getBrand() + "'" +
+            "}";
+    }
+
+}
