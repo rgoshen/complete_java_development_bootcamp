e3c0b0db168c8f315b62e087d1598a7b40c0fb7d
rgoshen <rick.goshen@gmail.com>
2021-10-20 11:59:40 -0700

    finished joining threads lesson

--- java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/joining-threads/Main.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/joining-threads/Main.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
     static final double TARGET = 0.5;
     static final double PRECISION = 0.000000001;
 
+    static double result = 0;
+
     public static void main(String[] args) {
 
-        //call generateNumber here...
-        //calculate precision level here...
-        System.out.println("The computer returned a value of: " + <result>);
-        System.out.println("The value was generated to a precision of : " + <precision>);
+        Runnable runnable = () -> result = generateNumber();
+        Thread thread = new Thread(runnable);
+        thread.start();
+        
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Please enter in a name to generate a number: ");
+        // scan.nextLine();
+        // scan.close();
+        
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+        
+        double precision = difference(result);
+        System.out.println("The computer returned a value of: " + result);
+        System.out.println("The value was generated to a precision of : " + precision);
     }
 
     /**
@@ -19,6 +37,20 @@ public class Main {
      *   1. Generates a number close to the TARGET to a precision of PRECISION.
      * 
      */
+    public static double generateNumber() {
+        double number = Math.random();
+        double difference = difference(number);
 
+        while (difference > PRECISION) {
+            number = Math.random();
+            difference = difference(number);
+        }
+
+        return number;
+    }
+
+    public static double difference(double number) {
+        return Math.abs(TARGET - number);
+    }
 
 }
