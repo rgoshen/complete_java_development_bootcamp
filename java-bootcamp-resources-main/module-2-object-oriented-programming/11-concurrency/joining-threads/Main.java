a10c01e756872fbd7bd7a4d7eefd38185b8b7d84
rgoshen <rick.goshen@gmail.com>
2021-10-20 16:52:32 -0700

    finished thread pool lesson

--- java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/joining-threads/Main.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/joining-threads/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class Main {
 
-    static final double TARGET = 0.5;
-    static final double PRECISION = 0.000000001;
+    // static final double TARGET = 0.5;
+    static double[] targets = new double[] {0.5, 0.8, 0.3};
+    // static final double PRECISION = 0.000000001;
+    static final double PRECISION = 0.0000001;
 
     static double result = 0;
 
@@ -13,28 +18,50 @@ public class Main {
 
         // Runnable runnable = () -> result = generateNumber();
         // Thread thread = new Thread(runnable);
-        Callable<Double> callable = () -> generateNumber();
-        FutureTask<Double> future = new FutureTask<>(callable);
-        Thread thread = new Thread(future);
+        // Callable<Double> callable = () -> generateNumber();
+        // FutureTask<Double> future = new FutureTask<>(callable);
 
-        thread.start();
-        
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Please enter in a name to generate a number: ");
-        scan.nextLine();
-        scan.close();
+        int numThreads = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        Future<Double> future = executor.submit(() -> generateNumber(0));
+        Future<Double> future2 =executor.submit(() -> generateNumber(1));
+        Future<Double> future3 =executor.submit(() -> generateNumber(2));
+
+        // FutureTask<Double> future = new FutureTask<>(() -> generateNumber(0));
+        // Thread thread = new Thread(future);
+        // FutureTask<Double> future2 = new FutureTask<>(() -> generateNumber(1));
+        // Thread thread2 = new Thread(future2);
+        // FutureTask<Double> future3 = new FutureTask<>(() -> generateNumber(2));
+        // Thread thread3 = new Thread(future3);
+
+        // thread.start();
+        // thread2.start();
+        // thread3.start();
+
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Please enter in a name to generate a number: ");
+        // scan.nextLine();
+        // scan.close();
         
         try {
             // thread.join();
-            result = future.get();
+            // result = future.get();
+            future.get();
+            future2.get();
+            future3.get();
         // } catch (InterruptedException e) {
         } catch (Exception e) {
             System.err.println(e.getMessage());
+        } finally {
+            executor.shutdown();
         }
         
-        double precision = difference(result);
-        System.out.println("The computer returned a value of: " + result);
-        System.out.println("The value was generated to a precision of : " + precision);
+        // double precision = difference(result);
+        // System.out.println("The computer returned a value of: " + result);
+        System.out.println("Finished running background operations.");
+        // System.out.println("The value was generated to a precision of : " + precision);
     }
 
     /**
@@ -45,20 +72,20 @@ public class Main {
      *   1. Generates a number close to the TARGET to a precision of PRECISION.
      * 
      */
-    public static double generateNumber() {
+    public static double generateNumber(int index) {
         double number = Math.random();
-        double difference = difference(number);
+        double difference = difference(number, index);
 
         while (difference > PRECISION) {
             number = Math.random();
-            difference = difference(number);
+            difference = difference(number, index);
         }
 
         return number;
     }
 
-    public static double difference(double number) {
-        return Math.abs(TARGET - number);
+    public static double difference(double number, int index) {
+        return Math.abs(targets[index] - number);
     }
 
 }
