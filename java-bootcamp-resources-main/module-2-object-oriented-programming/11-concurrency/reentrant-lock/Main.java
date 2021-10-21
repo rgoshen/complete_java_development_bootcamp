4b09745227599a30f0c6d9fa5b9b7b589fdc81d2
rgoshen <rick.goshen@gmail.com>
2021-10-20 17:40:55 -0700

    finished count down latch lesson

--- java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/reentrant-lock/Main.java
+++ java-bootcamp-resources-main/module-2-object-oriented-programming/11-concurrency/reentrant-lock/Main.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -6,30 +9,52 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ReentrantLock lock = new ReentrantLock();
-        Thread thread1 = new Thread(() -> task(lock));
-        Thread thread2 = new Thread(() -> task(lock));
+        int tries = 55;
 
-        thread1.start();
-        thread2.start();
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(nThreads);
+        CountDownLatch latch = new CountDownLatch(tries);
+        ReentrantLock lock = new ReentrantLock();
+        for (int i = 0; i < tries; i++) {
+            executor.submit(() -> task(lock, latch));
+        }
+        // executor.submit(() -> task(lock, latch));
+        // executor.submit(() -> task(lock, latch));
+        // executor.submit(() -> task(lock, latch));
+        // executor.submit(() -> task(lock, latch));
+        // Thread thread1 = new Thread(() -> task(lock, latch));
+        // Thread thread2 = new Thread(() -> task(lock, latch));
+        // Thread thread3 = new Thread(() -> task(lock, latch));
+        // Thread thread4 = new Thread(() -> task(lock, latch));
+
+        // thread1.start();
+        // thread2.start();
+        // thread3.start();
+        // thread4.start();
 
         try {
-            thread1.join();
-            thread2.join();
+            // thread1.join();
+            // thread2.join();
+            // thread3.join();
+            // thread4.join();
+            latch.await();
         } catch (Exception e) {
             System.err.println(e.getMessage());
+        } finally {
+            executor.shutdown();
         }
 
         System.out.println(counter);
     }
 
-    public static void task(ReentrantLock lock) {
+    public static void task(ReentrantLock lock, CountDownLatch latch) {
         //some really long operations that need to run in the background...
         for (int i = 0; i < 10000; i++) {
             lock.lock();
             counter++; // there happens to be a line that updates the counter variable.
             lock.unlock();
         }
+        latch.countDown();
         //more really long operations...
     }
 
